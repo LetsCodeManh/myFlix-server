be3c1c461b4d259c67389deebff6c5a19b9c2272
LetsCodeManh <manh.ng@icloud.com>
2023-01-15 17:26:19 +0100

    Create MongooseSchema User and Movie

---
+++ models.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+mongoose.connect("mongod://localhost:27017/dbname", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let movieSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  genre: {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+  },
+  director: {
+    name: { type: String, required: true },
+    bio: { type: String, required: true },
+  },
+  actors: [String],
+  imagePath: String,
+  featured: Boolean,
+});
+
+let userSchema = mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  birthday: Date,
+  favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "movie" }],
+});
+
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = (password) => {
+  return bcrypt.compareSync(password, this.password);
+};
+
+let movie = mongoose.model("movie", movieSchema);
+let user = mongoose.model("user", userSchema);
+
+module.exports.movie = movie;
+module.exports.user = user;
